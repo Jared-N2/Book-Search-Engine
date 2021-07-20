535bafb36284410a046478a7e277bfb403fa13c9
Jared Neisen <Jared.Neisen@gmail.com>
2021-07-20 10:11:09 -0600

    added mongoDB connection

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require ('./utils/auth');
+const  mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -13,9 +14,14 @@ const server = new ApolloServer({
 });
 server.applyMiddleware({app});
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
